8e4a1c08c7578811f152c6438d98019125c68274
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-07-16 00:22:53 +0300

    solved task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const resultObj = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(resultObj);
+  event.currentTarget.reset();
+}
