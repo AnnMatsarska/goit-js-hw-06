1b4492a1c392c411b4bd8bfc58912582f951e6b1
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-07-16 00:35:34 +0300

    solved task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const colorBtn = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+colorBtn.addEventListener('click', onClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onClick() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
 }
